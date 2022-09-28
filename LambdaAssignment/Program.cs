79d2f8a357009cc6d19a075c11c87ab19d379c81
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-28 18:09:12 +0530

    [Abhi]Added:Check Valid last name

--- LambdaAssignment/Program.cs
+++ LambdaAssignment/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        public static string firstname;
+        public static string firstname,lastname;
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose an option:\n 1.First Name ");
+            Console.WriteLine("Choose an option:\n 1.First Name \n 2.Last name");
             int result = Convert.ToInt32(Console.ReadLine());
 
             switch (result)
@@ -15,6 +15,12 @@
                     firstname = Console.ReadLine();
                     Pattern.CheckFirstName(firstname);
                     break;
+                case 2:
+                    Console.WriteLine("Enter your last name");
+                    lastname = Console.ReadLine();
+                    Pattern.CheckLastName(lastname);
+                    break;
+
             }
         }
     }
