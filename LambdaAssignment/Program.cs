1908a96be728d6a11e1cb3c3a6f3f0e7c2abbd8c
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-28 18:02:39 +0530

    [Abhi]Added:Check Valid first name

---
+++ LambdaAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LambdaAssignment
+{
+    internal class Program
+    {
+        public static string firstname;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an option:\n 1.First Name ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            switch (result)
+            {
+                case 1:
+                    Console.WriteLine("Enter your first name ");
+                    firstname = Console.ReadLine();
+                    Pattern.CheckFirstName(firstname);
+                    break;
+            }
+        }
+    }
+}
